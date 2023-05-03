003ccf470328d38fe3ff90fdc6862be64a99882d
Ayra Qutub <112740171+ayraqutub@users.noreply.github.com>
2023-05-03 12:39:46 -0600

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,6 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Load the MNIST dataset
+(x_train_full, y_train_full), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Normalize the data
+x_train_full = x_train_full.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+# Reshape the data to 4D arrays for use with convolutions
+x_train_full = x_train_full.reshape((len(x_train_full), 28, 28, 1))
+x_test = x_test.reshape((len(x_test), 28, 28, 1))
+
+# Convert the labels to one-hot encoded vectors
+y_train_full = keras.utils.to_categorical(y_train_full)
+y_test = keras.utils.to_categorical(y_test)
+
+# Split the training set into training and validation sets
+split_index = int(0.8 * len(x_train_full))
+x_train, x_val = x_train_full[:split_index], x_train_full[split_index:]
+y_train, y_val = y_train_full[:split_index], y_train_full[split_index:]
+
+# Define the model architecture
+model = keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile the model with categorical cross-entropy loss function and Adam optimizer
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model on the training data with a validation set
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val), verbose=2)
+
+# Evaluate the model on the test set
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_acc:.3f}')
+
+
 # Train the model on the training data with a validation set
 history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val), verbose=2)
 
